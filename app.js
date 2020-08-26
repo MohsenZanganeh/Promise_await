9751286edc4c650161be0f639ced9d149a7d6a0a
MYAGAH\M.Zangane <mohsen.1398.pg@gmail.com>
2020-08-26 11:00:15 +0430

    work with Promis step (Promis.all)

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 let express = require("express")
 let app = express()
 
-let prA=new Promise(resolve=>{
-    resolve("prA")
+let promisA = Promise.resolve(50)
+let promisB = 45;
+let PromisC = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("finish Timeout........")
+    }, 1000)
 })
-let prB =prA.then(result=>{
-       return `PromisA:${result}`
+
+Promise.all([promisA,promisB,PromisC]).then(velue=>{
+    console.log(velue)
 })
-prB.then(result=>{
-    console.log(`promis A ${result}`)
+
+let p1=Promise.all(["Hello","Mohsen 1111"])
+let p2=Promise.all(["Hello","Mohsen 2222"])
+let p3=Promise.all(["Hello","Mohsen 3333"])
+
+setTimeout(()=>{
+   console.log(p1)
+   console.log(p2)
+   console.log(p3)
 })
+
 app.listen(3800, () => {
     console.log("Connect To Server........")
 })
