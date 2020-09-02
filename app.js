3ca7864e694c92ed983f1fd6bf30c11d734ddd1b
MYAGAH\M.Zangane <mohsen.1398.pg@gmail.com>
2020-09-02 14:56:35 +0430

    create a promis chain and put the catch between two then

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-let express = require("express")
+let express = require("express");
+const { json } = require("express");
 let app = express()
-let i=0;
-   function login(){
-      return new Promise((resolve,reject)=>{
-           setTimeout(()=>{
-               try{
-                     resolve(`Finish Timeout .....${i++}`)
-               }
-               catch(err){
-                     reject(`Exeption ............ ${err} `)
-               }              
-           })
-       })
-   }
+let i = 0;
+function login(res) {
+    let a = new Promise((resolve, reject) => { 
+            resolve("5")
+    })
+   return a.then(result => {
+      return  s + "10"
+    }).then(result=>{
+        return result+"10"
+    }).catch(err=>{
+        if(err){
+           return err
+        }
+    }).then(result=>{
+        return result + "--55"
+    })
+}
 
-   app.get("/",async (req,res)=>{
-       res.send(await login())
-   })
+app.get("/", async (req, res) => {
+    const rr = await login(res).then((result) => {
+        res.send(result)
+    })
+
+    return `${rr} mohsen`
+
+})
+
+function error(res, err) {
+    res.send(err)
+}
 app.listen(3800, () => {
     console.log("Connect To Server........")
 })
