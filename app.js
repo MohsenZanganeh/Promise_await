7b63fda1d1f128d4dd8c813b023243cc9447117c
MYAGAH\M.Zangane <mohsen.1398.pg@gmail.com>
2020-09-02 15:06:52 +0430

    set catch to end of then and return non object

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ let express = require("express");
 const { json } = require("express");
 let app = express()
 let i = 0;
-function login(res) {
-    let a = new Promise((resolve, reject) => { 
-            resolve("5")
+async function login(res) {
+    let a = new Promise((resolve, reject) => {
+        resolve("5")
     })
-   return a.then(result => {
-      return  s + "10"
-    }).then(result=>{
-        return result+"10"
-    }).catch(err=>{
-        if(err){
-           return err
-        }
-    }).then(result=>{
-        return result + "--55"
-    })
+    return await a.then(result => {
+            return result + "10"
+        }).then(result => {
+            return result + "10"
+        }).then(result => {
+            return d + "--55"
+        }).catch(err => {
+            if (err) {
+                console.log("err:", err)
+                return err
+            }
+        })
 }
 
 app.get("/", async (req, res) => {
